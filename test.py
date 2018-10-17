c695b684079111488be47eee2d95ded5975c3d20
Young Han Lee <cpumaker@gmail.com>
2018-10-17 10:56:15 +0900

    review 의견 반영

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 def hello_world(name=None):
-    print("hello world!, {}".format(name))
+	if name != None:
+    	print("hello world!, {}".format(name))
+    else:
+    	print("hello world!")
 
 if __name__ == "__main__":
     hello_world()
